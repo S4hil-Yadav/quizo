9f8c41942e95c310baa14c60e43dfa72968c9d11
Sahil Yadav <sahilyadavind0908@gmail.com>
2025-02-18 16:35:50 +0530

    Update axios.ts

--- frontend/src/lib/axios.ts
+++ frontend/src/lib/axios.ts
@@ -2,7 +2,9 @@ import axios from "axios";
 
 const axiosInstance = axios.create({
   baseURL:
-    import.meta.env.MODE === "development" ? "http://localhost:3000" : "",
+    import.meta.env.MODE === "development"
+      ? "http://localhost:3000"
+      : import.meta.env.VITE_PRODUCTION_BASE_URL,
   withCredentials: true,
 });
 
